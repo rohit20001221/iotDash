8708d8672a8063181af5f9278db90764ef2959d5
rohit20001221 <rohit20001221@gmail.com>
2021-09-22 14:57:52 +0530

    chart component

--- components/widgets/LineChart.js
+++ components/widgets/LineChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Dimensions, View, Text } from "react-native";
 import {
   VictoryChart,
@@ -9,9 +9,20 @@ import {
 
 import tw from "tailwind-react-native-classnames";
 
+import { useMqtt } from "../../context/MqttContext";
+
 const window = Dimensions.get("window");
 
-const LineChart = ({ data, x, y, title }) => {
+const LineChart = ({ title, topic }) => {
+  const [data_, setData] = useState([]);
+
+  const { data } = useMqtt();
+
+  useEffect(() => {
+    console.log(data);
+    setData((oldData) => [...oldData, { x: data[topic], y: oldData.length }]);
+  }, [data]);
+
   return (
     <View style={tw`my-2 shadow-sm bg-white`}>
       <Text style={tw`text-lg font-bold`}>{title}</Text>
@@ -21,7 +32,7 @@ const LineChart = ({ data, x, y, title }) => {
         theme={VictoryTheme.material}
         containerComponent={<VictoryZoomContainer zoomDimension="x" />}
       >
-        <VictoryLine data={data} x={x} y={y} interpolation="linear" />
+        <VictoryLine data={data_} interpolation="linear" />
       </VictoryChart>
     </View>
   );
